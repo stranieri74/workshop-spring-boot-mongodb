3d746d0c0d2d316acc964574aff6a34158983ac6
add <stranieri74@gmail.com>
2021-06-19 17:32:50 -0300

    Obtendo um post por id

---
+++ src/main/java/com/nelioalves/workshopmongo/Services/PostService.java
@@ -0,0 +1,24 @@
+package com.nelioalves.workshopmongo.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.workshopmongo.Services.exception.ObjectNotFoundException;
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return  user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
+
+}
