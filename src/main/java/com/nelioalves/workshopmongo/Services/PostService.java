7fffbd8a116dc7bead5a55b290f45c84e15a9329
add <stranieri74@gmail.com>
2021-06-20 11:29:37 -0300

    Consulta Simples com Query Methods

--- src/main/java/com/nelioalves/workshopmongo/Services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/Services/PostService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.Services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class PostService {
 		Optional<Post> user = repo.findById(id);
 		return  user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
--- src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -1,14 +1,18 @@
 package com.nelioalves.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.workshopmongo.Services.PostService;
 import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.resources.util.URL;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -24,6 +28,15 @@ public class PostResource {
 		
 		//no corpo da resposta colocamos a lista
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	//passando o parametro sem ser por /
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){	
+		text = URL.decodeParam(text);
+		List<Post> list= service.findByTitle(text);
+		//no corpo da resposta colocamos a lista
+		return ResponseEntity.ok().body(list);
 	}
 		
 
